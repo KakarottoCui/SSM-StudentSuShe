d116a7e1d99a0a403b6a16f332eaf0c776c8f0bf
cuifengbo <471363336@qq.com>
2023-06-13 09:51:01 +0800

    1

---
+++ src/com/itheima/dao/VisitorDao.java
@@ -0,0 +1,25 @@
+package com.itheima.dao;
+
+import com.itheima.po.Visitor;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * @program: dormitorySystem
+ * @description: 访客
+ **/
+public interface VisitorDao {
+    /**
+     * 进行分页查询
+     */
+
+    //获取总条数
+    public Integer totalCount(@Param("v_name") String v_name, @Param("v_phone")Integer v_phone);
+    //获取用户列表
+    public List<Visitor> getVisitorList(@Param("v_name") String v_name, @Param("v_phone")Integer v_phone,@Param("currentPage")Integer currentPage, @Param("pageSize")Integer pageSize);
+
+    public int addVisitor(Visitor visitor);   //添加学生信息
+    public List<Visitor> getAll();
+
+}
---
+++ web/index.jsp
@@ -0,0 +1,9 @@
+
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+</head>
+<body>
+   <jsp:forward page="/WEB-INF/jsp/login.jsp"/>
+</body>
+</html>
